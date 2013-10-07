0e848929a5438635ba2f220130d34e7b94fb9c6e
soh-i <soh.ishiguro3914@gmail.com>
2013-10-07 19:59:23 +0900

    refactored to subroutines

--- metadata_aggregator.pl
+++ metadata_aggregator.pl
@@ -7,45 +7,51 @@ use LWP::UserAgent;
 use HTTP::Request::Common;
 use XML::Simple;
 
+
 my $ID = 'GSM914117';
-my $url = 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi';
-
-my %param = ('acc'  => $ID,
-             'form' => 'xml',
-             'view' => 'full'
-            );
-
-my $req = POST($url, \%param);
-my $ua = LWP::UserAgent->new();
-my $res = $ua->request($req);
-my $data = $res->content();
-
-my $xml = XML::Simple->new();
-my $row = $xml->XMLin($data);
-
-my $GSM = $row->{Sample}->{iid};
-my $sp = $row->{Sample}->{Channel}->{Organism}->{content};
-my $strain = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[0]->{content});
-my $tissue = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[1]->{content});
-my $age    = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[2]->{content});
-my $gender = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[3]->{content});
-
-#my $desc = _clean_up_data($row->{Sample}->{Description});
-
-my $lib_strategy = $row->{Sample}->{'Library-Strategy'};
-my $platform = $row->{Sample}->{'Instrument-Model'}->{Predefined};
-my $SRA = _get_SRX_ID($row->{Sample}->{Relation}->[0]->{target});
-
-print $GSM, $sp, $strain, $tissue, $age, $gender, $lib_strategy, $platform, $SRA;
+print retrieve_data($ID);
 
 
+sub __fetch_xml_from_GEO {
+    my $ID = shift;
+    
+    my $url = 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi';
+    my %param = ('acc'  => $ID,
+                 'form' => 'xml',
+                 'view' => 'full'
+                );
+
+    my $req = POST($url, \%param);
+    my $ua = LWP::UserAgent->new();
+    my $res = $ua->request($req);
+    my $data = $res->content();
+};
+
+sub retrieve_data {
+    my $GEO_ID = shift;
+    my $xml = XML::Simple->new();
+    my $row = $xml->XMLin(__fetch_xml_from_GEO($GEO_ID));
+
+    my $GSM = $row->{Sample}->{iid};
+    my $sp = $row->{Sample}->{Channel}->{Organism}->{content};
+    my $strain = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[0]->{content});
+    my $tissue = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[1]->{content});
+    my $age    = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[2]->{content});
+    my $gender = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[3]->{content});
+
+    my $lib_strategy = $row->{Sample}->{'Library-Strategy'};
+    my $platform = $row->{Sample}->{'Instrument-Model'}->{Predefined};
+    my $SRA = _get_SRX_ID($row->{Sample}->{Relation}->[0]->{target});
+
+    my $result = sprintf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $GSM, $sp, $strain, $tissue, $age, $gender, $lib_strategy, $platform, $SRA;
+    return $result;
+};
 
 sub _clean_up_data {
     my ($data) = @_;
-    
     if ($data) {
         (my $c = $data) =~ s/\n//g;
-        return chomp $c;
+        return $c;
     }
 };
 
