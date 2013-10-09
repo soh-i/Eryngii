0f77f878e05004e8f4a02c2a3a1d8f456531771e
soh-i <soh.ishiguro3914@gmail.com>
2013-10-09 13:41:45 +0900

    Add id validator for GSM and GSE

--- lib/Metadata/Utils.pm
+++ lib/Metadata/Utils.pm
@@ -5,15 +5,34 @@ use warnings;
 use Carp;
 
 use base q/Exporter/;
-our @EXPORT = qw/__clean_up_data/;
+our @EXPORT = qw/_clean_up_data
+                 _is_GSE
+                 _is_GSM
+                /;
 
-sub __clean_up_data {
+
+sub _is_GSE {
+    my $id = shift;
+    if ($id =~ m/^GSE\d{4}$/) {
+        return 1;
+    }
+}
+
+sub _is_GSM {
+    my $id = shift;
+    if ($id =~ m/^GSM\d{4}$/) {
+        return 1;
+    }
+}
+
+sub _clean_up_data {
     my ($data) = @_;
     if ($data) {
         (my $c = $data) =~ s/(\n)|(\s+$)|(^\s+)//g;
         return $c;
     }
 };
+
 
 
 1;
