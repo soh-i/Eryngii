c7bbb12899910ae14352e0dfd4879936d8285a91
soh-i <soh.ishiguro3914@gmail.com>
2013-10-09 15:08:21 +0900

    remove HTTP access and parsing XML to Utils class

--- lib/Metadata/GSE.pm
+++ lib/Metadata/GSE.pm
@@ -4,9 +4,6 @@ use strict;
 use warnings;
 use Carp;
 use Data::Dumper;
-use LWP::UserAgent;
-use HTTP::Request::Common;
-use XML::Simple;
 
 use Metadata::Utils;
 
@@ -22,34 +19,17 @@ sub new {
                 GSEID => $id,
                 URL => 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi',
                };
-
-    my %param = ('acc'  => $self->{GSEID},
+    
+    my $params = {'acc'  => $self->{GSEID},
                  'targ' => 'series',
                  'form' => 'xml',
                  'view' => 'full',
-                );
+                };
     
-    my $req = POST($self->{URL}, \%param);
-    my $ua = LWP::UserAgent->new();
-    my $res = $ua->request($req);
-    
-    my $data = ();
-    if ($res->is_success) {
-        $data = $res->content();
-    } elsif ($res->is_error()) {
-        croak("HTTP Request error: ", $res->status_line());
-    }
-    
-    my $xml = XML::Simple->new(forcearray=>0);
-    eval {
-        $self->{XML} = $xml->XMLin($data);
-    };
-    if ($@) {
-        croak("XML::Simple error: invalid XML content maybe given");
-    }
-    
+    $self->{XML} = _fetch_xml(id=>$self->{GSE_ID}, url=>$self->{URL}, post_params=>$params);
     bless $self, $class;
 }
+
 
 sub title {
     my $self = shift;
