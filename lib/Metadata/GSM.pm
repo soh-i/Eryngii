fb7eaff65c2ae4f7cf025d5c72b617f50c735bf7
soh-i <soh.ishiguro3914@gmail.com>
2013-10-09 15:41:52 +0900

    Make acccessors of GSM attributes

--- lib/Metadata/GSM.pm
+++ lib/Metadata/GSM.pm
@@ -6,7 +6,6 @@ use Carp;
 use LWP::Simple;
 use HTTP::Request::Common;
 use XML::Simple;
-
 use Metadata::Utils;
 
 
@@ -20,59 +19,65 @@ sub new {
                };
     
     my $url = 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi';
-    my %param = ('acc'  => $ID,
-                 'form' => 'xml',
-                 'view' => 'full'
-                );
-
-    my $req  = POST($url, \%param);
-    my $ua   = LWP::UserAgent->new();
-    my $res  = $ua->request($req);
-    my $data = $res->content();
     
-    my $xml = XML::Simple->new();
-    my $row = $xml->XMLin($data);
+    my $params = {'acc'  => $self->{GSMID},
+                'form' => 'xml',
+                'view' => 'full'
+               };
     
-    $self->{XML} = $row;
+    $self->{XML} = _fetch_xml(id=>$self->{GSMID}, url=>$self->{URL}, post_params=>$params);
     bless $self, $class;
 }
 
-sub fetch_xml_from_GSM {
-    my $ID = shift;
-    if (!$ID) {
-        croak("Error: GEO is not found");
-    }
+sub iid {
+    my $self = shift;
+    $self->{XML}->{Sample}->{iid};
+}
+
+sub sp {
+    my $self = shift;
+    $self->{XML}->{Sample}->{Channel}->{Organism}->{content};
+}
+
+sub strain {
+    my $self = shift;
+    _clean_up_data($self->{XML}->{Sample}->{Channel}->{Characteristics}->[0]->{content});
+}
+
+sub tissue {
+    my $self = shift;
+    _clean_up_data($self->{XML}->{Sample}->{Channel}->{Characteristics}->[1]->{content});
+}
+   
+sub age {
+    my $self = shift;
+    _clean_up_data($self->{XML}->{Sample}->{Channel}->{Characteristics}->[2]->{content});
+}
+
+sub gender {
+   my $self = shift;
+   _clean_up_data($self->{XML}->{Sample}->{Channel}->{Characteristics}->[3]->{content});
+}
+
+sub lib_strategy {
+    my $self = shift;
     
-    
-    my $GSM = $row->{Sample}->{iid};
-    my $sp = $row->{Sample}->{Channel}->{Organism}->{content};
-    my $strain = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[0]->{content});
-    my $tissue = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[1]->{content});
-    my $age    = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[2]->{content});
-    my $gender = _clean_up_data($row->{Sample}->{Channel}->{Characteristics}->[3]->{content});
+}
 
-    my $lib_strategy = $row->{Sample}->{'Library-Strategy'};
-    my $platform = $row->{Sample}->{'Instrument-Model'}->{Predefined};
-    my $SRA = _get_SRX_ID($row->{Sample}->{Relation}->[0]->{target});
+sub platform {
+   my $self = shift;
+   $self->{XML}->{Sample}->{'Instrument-Model'}->{Predefined};
+}
 
-    my $result = sprintf "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $GSM, $sp, $strain, $tissue, $age, $gender, $lib_strategy, $platform, $SRA;
-    return $result;
-};
-
-sub _clean_up_data {
-    my ($data) = @_;
-    if ($data) {
-        (my $c = $data) =~ s/(\n)|(\s+$)|(^\s+)//g;
-        return $c;
-    }
-};
+sub srx_ids {
+    my $self = shift;
+    _get_SRX_ID($self->{XML}->{Sample}->{Relation}->[0]->{target});
+}
 
 sub _get_SRX_ID {
     my ($url) = @_;
     $url =~ m/\?term\=(SRX\d+)$/;
     return $1;
-};
-
-
+}
 
 1;
