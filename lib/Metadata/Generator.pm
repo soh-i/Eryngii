3ed42508e4719aa736fe9e96c3bebc8f41c6cb19
soh-i <soh.ishiguro3914@gmail.com>
2013-10-09 22:38:05 +0900

    has a general class

--- lib/Metadata/Generator.pm
+++ lib/Metadata/Generator.pm
@@ -7,14 +7,40 @@ use Carp;
 use lib qw{./lib/};
 use Metadata::GEO::GSE;
 use Metadata::GEO::GSM;
-use Metadata::SRA::SRA;
 use Metadata::Utils;
 
 sub new {
-    my ($class, $args) = @_;
-    bless $class, $args;
+    my $class = shift;
+    my $id    = shift;
+    
+    if (not _is_GSE($id)) {
+        croak("Error: $id is invalid GSE ID");
+    }
+    
+    my $self = {
+                GSEID => $id,
+                URL => 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi',
+               };
+
+    my $params = {'acc'  => $self->{GSEID},
+                  'targ' => 'series',
+                  'form' => 'xml',
+                  'view' => 'full',
+                 };
+    
+    $self->{XML} = _fetch_xml(id=>$self->{GSE_ID}, url=>$self->{URL}, post_params=>$params);
+    bless $self, $class;
 }
 
+sub print_all {
+    my $self = shift;
+
+    
+}    
+
+
 1;
+
+
 
 
