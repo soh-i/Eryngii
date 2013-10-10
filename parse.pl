4e04ba83740cb202e996f619ba2d8c977d0159fd
soh-i <soh.ishiguro3914@gmail.com>
2013-10-10 21:52:11 +0900

    test for rodriguez paper's data

--- parse.pl
+++ parse.pl
@@ -1,14 +1,26 @@
 #!/usr/bin/env perl
+
 use strict;
 use warnings;
 use Data::Dumper;
+
 use lib qw{./lib};
 use Excel::Parser;
+use Generator::FruitFly::Rodriguez;
 
 my $ep = Excel::Parser->new(
-                            file=>"1471-2164-14-206-s5.xls",
-                            sheet=>"1471-2164-14-206-s5.csv",
-                            format=>'xls'
+                            file => "mmc1.xls",
+                            sheet => "Table_S1",
+                            format => 'xls'
                            );
 $ep->to_csv();
+
+my $gen = Generator::FruitFly::Rodriguez->parse($ep->file().'.csv');
+for my $id ($gen->iter()) {
+    print $gen->gene($id), "\t";
+    print $gen->position($id), "\t";
+    print $gen->chromosome($id), "\t";
+    print $gen->region($id), "\t";
+    print $gen->coverage($id), "\n";
+}
 
