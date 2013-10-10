b869875681987baef803ad7c5044871a87bdf8dc
soh-i <soh.ishiguro3914@gmail.com>
2013-10-10 14:34:21 +0900

    separated class by each study

---
+++ lib/Generator/FruitFly/Rodriguez.pm
@@ -0,0 +1,30 @@
+package Generator::FruitFly;
+
+use strict;
+use warnings;
+use Carp;
+use Excel::Parser;
+
+
+sub positon {
+}
+
+sub chromosome {
+}
+
+sub strand {
+}
+
+sub refseq {
+}
+
+sub genome_ver {
+}
+
+sub pmid { 
+}
+
+
+
+
+1;
