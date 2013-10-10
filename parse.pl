fc035d850c7daab541ed0831d161514fdf62fc3e
soh-i <soh.ishiguro3914@gmail.com>
2013-10-10 14:07:01 +0900

    Parsing xls

---
+++ parse.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+use lib qw{./lib};
+use Excel::Parser;
+
+my $ep = Excel::Parser->new(
+                            file=>"1471-2164-14-206-s5.xls",
+                            sheet=>"1471-2164-14-206-s5.csv",
+                            format=>'xls'
+                           );
+print $ep->file(), "\n";
+print $ep->sheet(), "\n";
+print $ep->to_csv();
