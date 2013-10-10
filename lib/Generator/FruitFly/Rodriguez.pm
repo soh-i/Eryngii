e2aab18bbccffcfa302da67fb6cdc9e6b8d7deff
soh-i <soh.ishiguro3914@gmail.com>
2013-10-10 20:19:04 +0900

    add data accessor and iter

--- lib/Generator/FruitFly/Rodriguez.pm
+++ lib/Generator/FruitFly/Rodriguez.pm
@@ -4,11 +4,6 @@ use strict;
 use warnings;
 use Carp;
 use IO::File;
-use Excel::Parser;
-
-sub generate_db {
-    
-}
 
 ### header info ###
 #CG ID
@@ -27,52 +22,69 @@ sub generate_db {
 #0.05pctile_editinglevel
 #Occurs in all small prep controls (yw and FM7a both replicates)
 
+# !!!CAUTION!!! change type of coord column to standard 
+# because parse error occured at contained "," in coord.
+
 sub parse {
-    my $self = shift;
+    my $class = shift;
+    my $file  = shift;
     
-    my $fh = IO::File->new($args{file}) or croak "Can not open file:$!";
-    my $collected = [];
-    while (my $data_entory = $bfh->getline()) {
+    my $self = ();
+    my $fh = IO::File->new($file) or croak "Internal Error: Can not open file:$!";
+    while (my $data_entory = $fh->getline) {
         next if __LINE__ == 1;
         my ($gene, $region, $chr, $pos, $cov, $pooled_edit)  = (split /\,/, $data_entory)[0,2,3,4,5,7];
-        $self->{gene} = $gene;
-        $self->{region} = $region;
-        $self->{chr} = $chr;
-        $self->{pos} = $pos;
-        $self->{coverage} = $cov;
-        $self->{nascent_edit_ratio} = $pooled_edit;
+        my $id = '_id'.int(rand(1000000));
+        push @{ $self->{$id}->{gene} },  $gene;
+        push @{ $self->{$id}->{region} },$region;
+        push @{ $self->{$id}->{chr} }, $chr;
+        push @{ $self->{$id}->{pos} }, $pos;
+        push @{ $self->{$id}->{coverage} }, $cov;
+        push @{ $self->{$id}->{nascent_edit_ratio} }, $pooled_edit;
     }
-    return $self;
+    $fh->close();
+    bless $self, __PACKAGE__;
+}
+
+sub iter {
+    my $self = shift;
+    return keys %$self;
 }
 
 sub gene {
     my $self = shift;
-    return $self->{gene};
+    my $id = shift;
+    return @{$self->{$id}->{gene}};
 }
 
 sub region {
     my $self = shift;
-    return $self->{region};
+    my $id = shift;
+    return @{$self->{$id}->{region}};
 }
 
 sub chromosome {
     my $self = shift;
-    return $self->{chr};
+    my $id = shift;
+    return @{$self->{$id}->{chr}};
 }
 
-sub positon {
+sub position {
     my $self = shift;
-    return $self->{pos};
+    my $id = shift;
+    return @{$self->{$id}->{pos}};
 }
 
 sub coverage {
     my $self = shift;
-    return $self->{coverage};
+    my $id = shift;
+    return @{$self->{$id}->{coverage}};
 }
 
 sub nascent_edit_ratio {
     my $self = shift;
-    return $self->{nascent_edit_ratio};
+    my $id = shift;
+    return @{$self->{$id}->{nascent_edit_ratio}};
 }
 
 sub AUTOLOAD {
